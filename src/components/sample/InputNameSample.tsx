fc45c019207f3351c1dba37f9f261424255ec1da
maeta187 <nexus18789@gmail.com>
2022-12-05 00:46:00 +0900

    ✨ : InputNameSampleの作成

---
+++ src/components/sample/InputNameSample.tsx
@@ -0,0 +1,31 @@
+// NOTE:動作確認ファイルのため今後削除する可能性あり
+import { useForm, SubmitHandler } from 'react-hook-form'
+
+type MyFromData = {
+  fistName: string
+  lastName: string
+  category: string
+}
+
+export default function InputNameSample() {
+  const { register, handleSubmit, formState: { errors }, } = useForm<MyFromData>()
+  const onSubmit: SubmitHandler<MyFromData> = (data) => {
+    console.log(data);
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input {...register('fistName', { required: true })} placeholder="名前" />
+      {errors.fistName && <div>名前を入力してください</div>}
+      <input {...register('lastName', { required: true })} placeholder="名字" />
+      {errors.lastName && <div>名字を入力してください</div>}
+      <select {...register('category', { required: true })}>
+        <option value="">選択</option>
+        <option value="A">カテゴリA</option>
+        <option value="B">カテゴリB</option>
+      </select>
+      {errors.category && <div>カテゴリを選択してください。</div>}
+      <input type="submit" />
+    </form>
+  )
+}
